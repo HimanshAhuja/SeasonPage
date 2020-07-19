bd8c3c8e1014333d3fabc82caddfd49ab7c94dba
HimanshAhuja <67634795+HimanshAhuja@users.noreply.github.com>
2020-07-19 10:05:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function copy() {
+  alert("LOL!! TRY NOT TO BE LAZY,TYPE THE CONTENT!!");
+}
+
+function summer() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/dkd5jyhic/image/upload/v1594968327/Summer_tf9yzu.png')";
+  document.body.style.backgroundRepeat = "no repeat";
+  document.body.style.backgroundSize = "100%";
+}
+
+function winter() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+  document.body.style.backgroundRepeat = "no repeat";
+  document.body.style.backgroundSize = "100%";
+}
+
+function autumn() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+  document.body.style.backgroundRepeat = "no repeat";
+  document.body.style.backgroundSize = "100%";
+}
+
+function spring() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+  document.body.style.backgroundRepeat = "no repeat";
+  document.body.style.backgroundSize = "100%";
+}
